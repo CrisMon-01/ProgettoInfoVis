da3d4c5306bd55711c8a464481de30e1ce230d37
Cristian Monti <cristian.monti@sourcesense.com>
2020-05-07 23:17:11 +0200

    f1 mancante muso

--- codice/index.js
+++ codice/index.js
@@ -1,15 +1,20 @@
 //const 
-const dims = { height: 900, width: 900 };
-const cent = { x: (dims.width / 2 + 5), y: (dims.height / 2 + 5)};
+const dims = { height: 2000, width: 900 };
+const margin = { heightmargin: 100, widthmargin:100 };
 
 //select body
 const body = d3.select("body");
 
 //add svg to show
-const svg = body.append("svg")
+const svgext = body.append("svg")
     .attr("style", "background-color:#d2d2cf")
     .attr("width", dims.width)
     .attr("height", dims.height);
+
+const svgint = svgext.append("svg")
+    .attr("width", (dims.width-margin.heightmargin))
+    .attr("height", (dims.height-margin.widthmargin));
+
 
 //check read json async uso callback
 d3.json('../dati/datimultivar.json').then( data => { 
@@ -21,19 +26,64 @@ d3.json('../dati/datimultivar.json').then( data => {
         .range([0,dims.height])  
     // console.log(y("99")) //OK
 
-    // svg.append('rect')  //OK
-
     // uso enter join e exit
-    const rects = svg.selectAll("rect").data(data);
+    const shapes = svgint.selectAll("rect").data(data);
 
     // append rects enter selection al DOM
-    rects.enter()   
+    //alettone ant
+    shapes.enter()  
         .append("rect")
-            .attr("x", 10)
-            .attr("y", d => ( y(d.numero) + 20 ) )    //OK
+            .attr("x", 55 )
+            .attr("y", d => ( y(d.numero) + 10 ) )    //OK
+            .attr("width", 40)
+            .attr("height", 20)
+            .attr('fill','black');
+    //muso
+    // shapes.enter()
+    //     .append('path')
+    //         .attr("d", d3.svg.symbol().type("triangle-up").size(10))
+    //         .attr("transform", function(d) { return "translate(" + 10 + "," + 10 + ")"; })
+    //         .style("fill", "red");
+    //corpo
+    shapes.enter()
+        .append("rect")
+            .attr("x", 50)
+            .attr("y", d => ( y(d.numero) + 50 ) )    
             .attr("width", 50)
             .attr("height", 50)
-            .attr('fill','black');
+            .attr('fill','white');
+    //ruota up sx
+    shapes.enter()
+        .append("rect")
+            .attr("x", 35)
+            .attr("y", d => ( y(d.numero) + 50 ) ) 
+            .attr("width", 15)
+            .attr("height", 15)
+            .attr('fill','grey');
+    //ruota up dx
+    shapes.enter()
+        .append("rect")
+            .attr("x", 100)
+            .attr("y", d => ( y(d.numero) + 50 ) ) 
+            .attr("width", 15)
+            .attr("height", 15)
+            .attr('fill','grey');
+    //ruota down sx
+    shapes.enter()
+        .append("rect")
+            .attr("x", 35)
+            .attr("y", d => ( y(d.numero) + 85 ) ) 
+            .attr("width", 15)
+            .attr("height", 15)
+            .attr('fill','grey');
+    // ruota down dx
+    shapes.enter()
+        .append("rect")
+            .attr("x", 100)
+            .attr("y", d => ( y(d.numero) + 85 ) ) 
+            .attr("width", 15)
+            .attr("height", 15)
+            .attr('fill','grey');
 
 });
 
@@ -43,10 +93,10 @@ d3.json('../dati/datimultivar.json').then( data => {
 // scale.rande([10, dims.width]);
 
 // legend setup
-// const legendGroup = svg.append('g')
-//   .attr('transform', `translate(${dims.width + 40}, 10)`)
+const legendGroup = svgext.append('g')
+  .attr('transform', `translate(${dims.width + 40}, 10)`)
 
-// const legend = d3.legendColor()
-//   .shape('path', d3.symbol().type(d3.symbolCircle)())
-//   .shapePadding(10)
-//   .scale(colour)
+const legend = d3.legendColor()
+  .shape('path', d3.symbol().type(d3.symbolCircle)())
+  .shapePadding(10)
+  .scale(colour)
