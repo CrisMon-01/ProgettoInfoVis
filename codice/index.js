2f8022132997a6cc95ae35841b8369295e3815c6
Cristian Monti <cristian.monti@sourcesense.com>
2020-05-13 12:02:04 +0200

    aggiunti contorni forme e colorazione unica macchine

--- codice/index.js
+++ codice/index.js
@@ -1,5 +1,5 @@
 //const 
-const dims = { height: 2300, width: 900 };
+const dims = { height: 2300, width: 900 , larghbody: 50, lungh: 60, larghwheel: 15 , lugnhwheel: 15 };
 const margin = { heightmargin: 10, widthmargin:100 };
 
 //select body
@@ -11,37 +11,37 @@ const svg = body.append("svg")
     .attr("id", "ext")
     .attr("width", dims.width)
     .attr("height", dims.height)
-    .append("svg")
-    .attr("id","int")
-    .attr("width", (dims.width-margin.heightmargin))
-    .attr("height", (dims.height-margin.widthmargin));
+    // .append("svg")
+    // .attr("id","int")
+    // .attr("width", (dims.width-margin.heightmargin))
+    // .attr("height", (dims.height-margin.widthmargin));
 
 //check read json async uso callback
 d3.json('../dati/datimultivar.json').then( data => { 
 
+    // setup y coordinate for each car
     const y = d3.scaleBand()
         .domain(data.map( datapoint => datapoint.numero ))
         .range([0,(dims.height-margin.heightmargin)]) 
+
+    // color setup for each car 
+    const colour = d3.scaleOrdinal(d3["schemeSet3"])
+        .domain(data.map( datapoint => datapoint.numero ))
 
     const shapes = svg.selectAll('svg').data(data);
 
     const groups = shapes.enter().append('g');
 
-    // body
-    groups.append('rect')
-        .attr("x", 50)
-        .attr("y",  d => ( y(d.numero) + 50 ) )
-        .attr("width", 50)
-        .attr("height", 60)
-        .attr('fill','white');
-
     //front wing
     groups.append('rect')
         .attr('x',55)
         .attr('y',  d => ( y(d.numero) + 15 ))
-        .attr('width', 40)
+        .attr('width', (dims.larghbody - 10))
         .attr('height', 15)
-        .attr('fill', 'black');
+        .attr('fill', d => ( colour(d.numero) ))
+        .attr('stroke','black')
+        .attr('stroke-width', '1')
+        .attr('id','frontwing');
 
     // muzzle
     groups.append('rect')
@@ -49,39 +49,96 @@ d3.json('../dati/datimultivar.json').then( data => {
         .attr('y',  d => ( y(d.numero) + 30 ))
         .attr('width', 15)
         .attr('height',20)
-        .attr('fill','grey');
+        .attr('fill', d => ( colour(d.numero) ))
+        .attr('stroke','black')
+        .attr('stroke-width', '1')
+        .attr('id','muso');
+
+    // body
+    groups.append('rect')
+        .attr("x", 50)
+        .attr("y",  d => ( y(d.numero) + 50 ) )
+        .attr("width", dims.larghbody)
+        .attr("height", 60)
+        .attr('fill', d => ( colour(d.numero) ))
+        .attr('stroke','black')
+        .attr('stroke-width', '1')
+        .attr('id','body');
+
+    // back wing
+    groups.append('rect')
+        .attr("x", 50)
+        .attr("y",  d => ( y(d.numero) + 120 ) )
+        .attr("width", dims.larghbody)
+        .attr("height", 15)
+        .attr('fill', d => ( colour(d.numero) ))
+        .attr('stroke','black')
+        .attr('stroke-width', '1')
+        .attr('id','backwing');
+
+    // connection back wing sn
+    groups.append('line')
+        .attr('x1',51)
+        .attr('y1', d => ( y(d.numero) + 110 ))
+        .attr('x2',51)
+        .attr('y2', d => ( y(d.numero) + 120 ))
+        .attr('stroke','black')
+        .attr('stroke-width','2')
+        .attr('id','connectionbackwingsn');
+
+    // connection back wing dx
+    groups.append('line')
+        .attr('x1',99)
+        .attr('y1', d => ( y(d.numero) + 110 ))
+        .attr('x2',99)
+        .attr('y2', d => ( y(d.numero) + 120 ))
+        .attr('stroke','black')
+        .attr('stroke-width','2')
+        .attr('id','connectionbackwingdx');        
 
     // wheel up sn
     groups.append("rect")
-    .attr("x", 30)
-    .attr("y",  d => ( y(d.numero) + 50 ) ) 
-    .attr("width", 15)
-    .attr("height", 15)
-    .attr('fill','grey');
+        .attr("x", 30)
+        .attr("y",  d => ( y(d.numero) + 50 ) ) 
+        .attr("width", dims.larghwheel)
+        .attr("height", dims.lugnhwheel)
+        .attr('fill','grey')
+        .attr('stroke','black')
+        .attr('stroke-width', '1')
+        .attr('id','ruotaupsn');
 
     // wheel up dx
     groups.append("rect")
         .attr("x", 105)
         .attr("y",  d => ( y(d.numero) + 50 ) ) 
-        .attr("width", 15)
-        .attr("height", 15)
-        .attr('fill','grey');
+        .attr("width", dims.larghwheel)
+        .attr("height", dims.lugnhwheel)
+        .attr('fill','grey')
+        .attr('stroke','black')
+        .attr('stroke-width', '1')
+        .attr('id','ruotaupdx');
 
     // wheel down sn
     groups.append("rect")
         .attr("x", 30)
         .attr("y",  d => ( y(d.numero) + 95 )) 
-        .attr("width", 15)
-        .attr("height", 15)
-        .attr('fill','grey');
+        .attr("width", dims.larghwheel)
+        .attr("height", dims.lugnhwheel)
+        .attr('fill','grey')
+        .attr('stroke','black')
+        .attr('stroke-width', '1')
+        .attr('id','ruotadownsn');
 
     // wheel down dx
     groups.append("rect")
-    .attr("x", 105)
-    .attr("y",  d => ( y(d.numero) + 95 ) ) 
-    .attr("width", 15)
-    .attr("height", 15)
-    .attr('fill','grey');
+        .attr("x", 105)
+        .attr("y",  d => ( y(d.numero) + 95 ) ) 
+        .attr("width", dims.larghwheel)
+        .attr("height", dims.lugnhwheel)
+        .attr('fill','grey')
+        .attr('stroke','black')
+        .attr('stroke-width', '1')
+        .attr('id','ruotadowndx');
 
     // wheel arm sn down
     groups.append('line')
@@ -90,7 +147,8 @@ d3.json('../dati/datimultivar.json').then( data => {
         .attr('x2',50)
         .attr('y2', d => ( y(d.numero) + 102 ))
         .attr('stroke','black')
-        .attr('stroke-width',5);
+        .attr('stroke-width',5)
+        .attr('id','bracciodownsn');
 
     // wheel arm sn up
     groups.append('line')
@@ -99,37 +157,27 @@ d3.json('../dati/datimultivar.json').then( data => {
         .attr('x2',50)
         .attr('y2', d => ( y(d.numero) + 57 ))
         .attr('stroke','black')
-        .attr('stroke-width',5);
+        .attr('stroke-width',5)
+        .attr('id','braccioupsn');
 
     // wheel arm dx down
     groups.append('line')
-    .attr('x1',100)
-    .attr('y1', d => ( y(d.numero) + 102 ))
-    .attr('x2',105)
-    .attr('y2', d => ( y(d.numero) + 102 ))
-    .attr('stroke','black')
-    .attr('stroke-width',5);
+        .attr('x1',100)
+        .attr('y1', d => ( y(d.numero) + 102 ))
+        .attr('x2',105)
+        .attr('y2', d => ( y(d.numero) + 102 ))
+        .attr('stroke','black')
+        .attr('stroke-width',5)
+        .attr('id','bracciodowndx');
 
     // wheel arm dx up
     groups.append('line')
-    .attr('x1',100)
-    .attr('y1', d => ( y(d.numero) + 57 ))
-    .attr('x2',105)
-    .attr('y2', d => ( y(d.numero) + 57 ))
-    .attr('stroke','black')
-    .attr('stroke-width',5);
+        .attr('x1',100)
+        .attr('y1', d => ( y(d.numero) + 57 ))
+        .attr('x2',105)
+        .attr('y2', d => ( y(d.numero) + 57 ))
+        .attr('stroke','black')
+        .attr('stroke-width',5)
+        .attr('id','braccioupdx');
 
 });
-
-// var scale = d3.linearScale();
-// scale.domain([0, d3.max(data, d => parseInt(d.))]);
-// scale.rande([10, dims.width]);
-
-// //legend setup
-// const legendGroup = svgext.append('g')
-//   .attr('transform', `translate(${dims.width + 40}, 10)`)
-
-// const legend = d3.legendColor()
-//   .shape('path', d3.symbol().type(d3.symbolCircle)())
-//   .shapePadding(10)
-//   .scale(colour)
