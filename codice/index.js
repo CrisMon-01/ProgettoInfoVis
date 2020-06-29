1917d2d5201005106dbe26c55a068fafa457ee1b
Cristian Monti <cristian.monti@sourcesense.com>
2020-06-29 19:05:04 +0200

    aggiunte modifiche richieste

--- codice/index.js
+++ codice/index.js
@@ -1,6 +1,6 @@
 //const 
-const dims = { height: 2300, width: 900 , larghbody: 50, lungh: 60, larghwheel: 15 , lugnhwheel: 15 };
-const margin = { heightmargin: 10, widthmargin:150 };
+const dims = { height: 1100, width: 1300 , larghbody: 70, lungh: 60, larghwheel: 15 , lugnhwheel: 15 };
+const margin = { heightmargin: 40, widthmargin:150 };
 
 //select body
 const body = d3.select("body");
@@ -24,108 +24,107 @@ d3.json('../dati/datimultivar.json').then( data => {
     const colour = d3.scaleOrdinal(d3["schemeSet3"])
         .domain(data.map( datapoint => datapoint.numero ))
 
+    const lungheach = d3.scaleLinear()
+        .domain([d3.min(data,d => d.lunghezza),d3.max(data,d => d.lunghezza)])
+        .range([15,35])
+
+    const larghwheel1 = d3.scaleLinear()
+        .domain([d3.min(data,d => d.dimensione_ruote),d3.max(data,d => d.dimensione_ruote)])
+        .range([10,16])
+
+    const spacebetweenwheel = d3.scaleLinear()
+        .domain([0,d3.max(data,d => d.distanza_delle_ruote)])
+        .range([0,50])
+
     // shift on x by one field
     const shift_by_lunghezza = d3.scaleLinear()
         .domain([0, d3.max(data,d=>d.lunghezza)])    
-        .range([0, dims.width-margin.widthmargin]);
+        .range([0, dims.width-margin.widthmargin-50]);
 
     const shift_by_dimensione_ruote = d3.scaleLinear()
         .domain([0, d3.max(data,d=>d.dimensione_ruote) ])
-        .range([0, dims.width-margin.widthmargin]);    
+        .range([0, dims.width-margin.widthmargin-50]);    
 
     const shift_by_distanza_delle_ruote = d3.scaleLinear()
         .domain([0, d3.max(data,d=>d.distanza_delle_ruote) ])
-        .range([0, dims.width-margin.widthmargin]);
+        .range([0, dims.width-margin.widthmargin-50]);
 
     const shift_by_peso = d3.scaleLinear()
         .domain([0, d3.max(data,d=>d.peso) ])
-        .range([0, dims.width-margin.widthmargin]);
+        .range([0, dims.width-margin.widthmargin-50]);
 
     const shapes = svg.selectAll('svg').data(data);
 
     const groups = shapes.enter().append('g')
         .attr('id', d => 'car'+d.numero )
-            .on('mouseover', handleMouseOver);    
-
-    //muzzle
-    groups.append('rect')
-        .attr('x', 67)
-        .attr('y',  d => ( y(d.numero) + 30 ))
-        .attr('width', 15)
-        .attr('height',20)
-        .attr('fill', d => ( colour(d.numero) ))
-        .attr('stroke','black')
-        .attr('stroke-width', '1')
-        .attr('number', d => d.numero )        
-        .attr('id','muso');
-
-    //front wing
-    groups.append('rect')
-        .attr('x',55)
-        .attr('y',  d => ( y(d.numero) + 15 ))
-        .attr('width', (dims.larghbody - 10))
-        .attr('height', 15)
-        .attr('fill', d => ( colour(d.numero) ))
-        .attr('stroke','black')
-        .attr('stroke-width', '1')
-        .attr('id','frontwing')
-        .attr('number', d => d.numero )
-            .on('click', transitionByLength.bind(this));
-
-
-    // body
-    groups.append('rect')
-        .attr("x", 50)
-        .attr("y",  d => ( y(d.numero) + 50 ) )
-        .attr("width", dims.larghbody)
-        .attr("height", 60)
-        .attr('fill', d => ( colour(d.numero) ))
-        .attr('stroke','black')
-        .attr('stroke-width', '1')
-        .attr('number', d => d.numero )
-        .attr('id','body')
-            .on('click', transitionByWeight.bind(this));
+            .on('mouseover', handleMouseOver);  
 
     // back wing
     groups.append('rect')
-        .attr("x", 50)
-        .attr("y",  d => ( y(d.numero) + 120 ) )
-        .attr("width", dims.larghbody)
-        .attr("height", 15)
+        .attr("x", 10)
+        .attr("y",  d => ( y(d.numero) + 20 ) )
+        .attr("width", d =>  lungheach(d.lunghezza) )
+        .attr("height", 50 )
         .attr('fill', d => ( colour(d.numero) ))
         .attr('stroke','black')
         .attr('stroke-width', '1')
         .attr('number', d => d.numero )
         .attr('id','backwing');
 
-    // connection back wing sn
+    // connection back wing down
     groups.append('line')
-        .attr('x1',51)
-        .attr('y1', d => ( y(d.numero) + 110 ))
-        .attr('x2',51)
-        .attr('y2', d => ( y(d.numero) + 120 ))
+        .attr('x1',d => (lungheach(d.lunghezza) + 10) )
+        .attr('y1',d => ( y(d.numero) + 60 ))
+        .attr('x2',d => (lungheach(d.lunghezza) + 20) )
+        .attr('y2',d => ( y(d.numero) + 60 ))
         .attr('stroke','black')
         .attr('stroke-width','2')
         .attr('number', d => d.numero )
         .attr('id','connectionbackwingsn');
 
-    // connection back wing dx
+    // connection back wing up
     groups.append('line')
-        .attr('x1',99)
-        .attr('y1', d => ( y(d.numero) + 110 ))
-        .attr('x2',99)
-        .attr('y2', d => ( y(d.numero) + 120 ))
+        .attr('x1',d => (lungheach(d.lunghezza) + 10) )
+        .attr('y1',d => ( y(d.numero) + 30 ))
+        .attr('x2',d => (lungheach(d.lunghezza) + 20) )
+        .attr('y2',d => ( y(d.numero) + 30 ))
         .attr('stroke','black')
         .attr('stroke-width','2')
         .attr('number', d => d.numero )
-        .attr('id','connectionbackwingdx');        
+        .attr('id','connectionbackwingsn'); 
 
     // wheel up sn
     groups.append("rect")
-        .attr("x", 30)
-        .attr("y",  d => ( y(d.numero) + 50 ) ) 
-        .attr("width", dims.larghwheel)
-        .attr("height", dims.lugnhwheel)
+        .attr("x", d => (lungheach(d.lunghezza) + 20))
+        .attr("y", d => ( y(d.numero) + 5 ) )
+        .attr("width", d => ( larghwheel1(d.dimensione_ruote) ))
+        .attr("height", dims.larghwheel )
+        .attr('fill','grey')
+        .attr('stroke','black')
+        .attr('stroke-width', '1')
+        .attr('number', d => d.numero )
+        .attr('id','ruotaupsn')
+            .on('click', transitionByWheelDim.bind(this));
+
+    // wheel down sn
+    groups.append("rect")
+        .attr("x", d => (lungheach(d.lunghezza) + 20))
+        .attr("y", d => ( y(d.numero) + 50 + 5 + dims.larghwheel ) )
+        .attr("width", d => ( larghwheel1(d.dimensione_ruote) ))
+        .attr("height", dims.larghwheel )
+        .attr('fill','grey')
+        .attr('stroke','black')
+        .attr('stroke-width', '1')
+        .attr('number', d => d.numero )
+        .attr('id','ruotaupsn')
+            .on('click', transitionByWheelDim.bind(this));
+
+    // wheel up dx
+    groups.append("rect")
+        .attr("x", d => (lungheach(d.lunghezza)+spacebetweenwheel(d.distanza_delle_ruote) + 20))
+        .attr("y", d => ( y(d.numero) + 5 ) )
+        .attr("width", d => ( larghwheel1(d.dimensione_ruote) ))
+        .attr("height", dims.larghwheel)
         .attr('fill','grey')
         .attr('stroke','black')
         .attr('stroke-width', '1')
@@ -133,129 +132,97 @@ d3.json('../dati/datimultivar.json').then( data => {
         .attr('id','ruotaupsn')
             .on('click', transitionByDistanceWhell.bind(this));
 
-    // wheel up dx
-    groups.append("rect")
-        .attr("x", 105)
-        .attr("y",  d => ( y(d.numero) + 50 ) ) 
-        .attr("width", dims.larghwheel)
-        .attr("height", dims.lugnhwheel)
-        .attr('fill','grey')
-        .attr('stroke','black')
-        .attr('stroke-width', '1')
-        .attr('number', d => d.numero )
-        .attr('id','ruotaupdx')
-            .on('click', transitionByDistanceWhell.bind(this));
-
-    // wheel down sn
-    groups.append("rect")
-        .attr("x", 30)
-        .attr("y",  d => ( y(d.numero) + 95 )) 
-        .attr("width", dims.larghwheel)
-        .attr("height", dims.lugnhwheel)
-        .attr('fill','grey')
-        .attr('stroke','black')
-        .attr('stroke-width', '1')
-        .attr('number', d => d.numero )
-        .attr('id','ruotadownsn')
-            .on('click', transitionByWheelDim.bind(this));
-
     // wheel down dx
     groups.append("rect")
-        .attr("x", 105)
-        .attr("y",  d => ( y(d.numero) + 95 ) ) 
-        .attr("width", dims.larghwheel)
-        .attr("height", dims.lugnhwheel)
+        .attr("x", d => (lungheach(d.lunghezza)+spacebetweenwheel(d.distanza_delle_ruote) + 20))
+        .attr("y", d => ( y(d.numero) + 50 + 5 + dims.larghwheel ) )
+        .attr("width", d => ( larghwheel1(d.dimensione_ruote) ))
+        .attr("height", dims.larghwheel )
         .attr('fill','grey')
         .attr('stroke','black')
         .attr('stroke-width', '1')
         .attr('number', d => d.numero )
-        .attr('id','ruotadowndx')
-            .on('click', transitionByWheelDim.bind(this));
+        .attr('id','ruotaupsn')
+            .on('click', transitionByDistanceWhell.bind(this));
 
-    // wheel arm sn down
-    groups.append('line')
-        .attr('x1',45)
-        .attr('y1', d => ( y(d.numero) + 102 ))
-        .attr('x2',50)
-        .attr('y2', d => ( y(d.numero) + 102 ))
+    // body
+    groups.append('rect')
+        .attr("x", d => (lungheach(d.lunghezza) + 20))
+        .attr("y",  d => ( y(d.numero) + 20 ) )
+        .attr("width", dims.larghbody)
+        .attr("height", 50)
+        .attr('fill', d => ( colour(d.numero) ))
         .attr('stroke','black')
-        .attr('stroke-width',5)
+        .attr('stroke-width', '1')
         .attr('number', d => d.numero )
-        .attr('id','bracciodownsn');
+        .attr('id','body')
+            .on('click', transitionByWeight.bind(this));
 
-    // wheel arm sn up
-    groups.append('line')
-        .attr('x1',45)
-        .attr('y1', d => ( y(d.numero) + 57 ))
-        .attr('x2',50)
-        .attr('y2', d => ( y(d.numero) + 57 ))
+    //muzzle
+    groups.append('rect')
+        .attr('x', d => (lungheach(d.lunghezza)+20+dims.larghbody))
+        .attr('y',  d => ( y(d.numero) + 35 ))
+        .attr('width', 25)
+        .attr('height',20)
+        .attr('fill', d => ( colour(d.numero) ))
         .attr('stroke','black')
-        .attr('stroke-width',5)
-        .attr('number', d => d.numero )
-        .attr('id','braccioupsn');
-
-    // wheel arm dx down
-    groups.append('line')
-        .attr('x1',100)
-        .attr('y1', d => ( y(d.numero) + 102 ))
-        .attr('x2',105)
-        .attr('y2', d => ( y(d.numero) + 102 ))
+        .attr('stroke-width', '1')
+        .attr('number', d => d.numero )        
+        .attr('id','muso');
+            
+    //front wing
+    groups.append('rect')
+        .attr('x',  d => (lungheach(d.lunghezza)+40+dims.larghbody) )
+        .attr('y',  d => ( y(d.numero) + 20 ))
+        .attr('width', d => (lungheach(d.lunghezza)/2+5 ))
+        .attr('height', 50)
+        .attr('fill', d => ( colour(d.numero) ))
         .attr('stroke','black')
-        .attr('stroke-width',5)
+        .attr('stroke-width', '1')
+        .attr('id','frontwing')
         .attr('number', d => d.numero )
-        .attr('id','bracciodowndx');
+            .on('click', transitionByLength.bind(this));
 
-    // wheel arm dx up
-    groups.append('line')
-        .attr('x1',100)
-        .attr('y1', d => ( y(d.numero) + 57 ))
-        .attr('x2',105)
-        .attr('y2', d => ( y(d.numero) + 57 ))
-        .attr('stroke','black')
-        .attr('stroke-width',5)
-        .attr('number', d => d.numero )
-        .attr('id','braccioupdx');
-
-    function transitionByLength(data) {
-        d3.json('../dati/datimultivar.json').then( data => { 
-        for (i in data){
-            body.selectAll('#car'+data[i].numero)
-                .transition().duration(400)
-                 .attr('transform', 'translate('+shift_by_lunghezza(data[i].lunghezza)+',0)');  //return (data.distanza_delle_ruote)
+        function transitionByLength(data) {
+            d3.json('../dati/datimultivar.json').then( data => { 
+            for (i in data){
+                body.selectAll('#car'+data[i].numero)
+                    .transition().duration(400)
+                     .attr('transform', 'translate('+shift_by_lunghezza(data[i].lunghezza)+',0)');  //return (data.distanza_delle_ruote)
+            }
+            });
         }
-        });
-    }
-
-    function transitionByWheelDim(data) {
-        d3.json('../dati/datimultivar.json').then( data => { 
-        for (i in data){
-            body.selectAll('#car'+data[i].numero)
-                .transition().duration(400)
-                 .attr('transform', 'translate('+shift_by_dimensione_ruote(data[i].dimensione_ruote)+',0)');  //return (data.distanza_delle_ruote)
+    
+        function transitionByWheelDim(data) {
+            d3.json('../dati/datimultivar.json').then( data => { 
+            for (i in data){
+                body.selectAll('#car'+data[i].numero)
+                    .transition().duration(400)
+                     .attr('transform', 'translate('+shift_by_dimensione_ruote(data[i].dimensione_ruote)+',0)');  //return (data.distanza_delle_ruote)
+            }
+            });
         }
-        });
-    }
-
-    function transitionByDistanceWhell(data) {
-        d3.json('../dati/datimultivar.json').then( data => { 
-        for (i in data){
-            body.selectAll('#car'+data[i].numero)
-                .transition().duration(400)
-                 .attr('transform', 'translate('+shift_by_distanza_delle_ruote(data[i].distanza_delle_ruote)+',0)');  //return (data.distanza_delle_ruote)
+    
+        function transitionByDistanceWhell(data) {
+            d3.json('../dati/datimultivar.json').then( data => { 
+            for (i in data){
+                body.selectAll('#car'+data[i].numero)
+                    .transition().duration(400)
+                     .attr('transform', 'translate('+shift_by_distanza_delle_ruote(data[i].distanza_delle_ruote)+',0)');  //return (data.distanza_delle_ruote)
+            }
+            });
         }
-        });
-    }
-
-
-    function transitionByWeight(data) {
-        d3.json('../dati/datimultivar.json').then( data => { 
-        for (i in data){
-            body.selectAll('#car'+data[i].numero)
-                .transition().duration(400)
-                 .attr('transform', 'translate('+shift_by_peso(data[i].peso)+',0)');  //return (data.distanza_delle_ruote)
+    
+    
+        function transitionByWeight(data) {
+            d3.json('../dati/datimultivar.json').then( data => { 
+            for (i in data){
+                body.selectAll('#car'+data[i].numero)
+                    .transition().duration(400)
+                     .attr('transform', 'translate('+shift_by_peso(data[i].peso)+',0)');  //return (data.distanza_delle_ruote)
+            }
+            });
         }
-        });
-    }
     
 });
 
